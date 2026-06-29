31a3306b818748a7d39ac831747a20b082848bc4
germancq <germancq@debianGCQ.dte.us>
2026-06-29 13:51:14 +0200

    updated to newer cocotb versions

--- ecb_mode/cocotb/simon_cte_test.py
+++ ecb_mode/cocotb/simon_cte_test.py
@@ -31,13 +31,13 @@ async def test(dut, index=0):
 
     for i in range(0, 5):
         assert (
-            dut.Z[i].value == simon.Z[i]
+            int(dut.Z[i].value) == simon.Z[i]
         ), f"ERROR with Z{i}, with N = {N} and M={M}, expected_value = {simon.Z[i]} and calculated = {dut.Z[i].value}"
 
     assert (
-        dut.Z_sel.value == simon.Z_sel[N][M]
+        int(dut.Z_sel.value) == simon.Z_sel[N][M]
     ), f"ERROR with Z_sel, with N = {N} and M = {M}, expected_value = {simon.Z_sel[N][M]} and calculated = {dut.Z_sel.value}"
 
     assert (
-        dut.T.value == simon.T[N][M]
+        int(dut.T.value) == simon.T[N][M]
     ), f"ERROR with T, with N = {N} and M = {M}, expected_value = {simon.T[N][M]} and calculated = {dut.T.value}"
